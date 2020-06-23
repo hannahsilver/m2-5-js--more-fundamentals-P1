9ab2d100b323b345b892f4f356d99170f714e2ba
Hannah Silver <hannah_silver@hotmail.com>
2020-06-23 09:12:26 -0400

    exercise 2-4

--- workshop/exercises-P2_tests/exercise-2-4.js
+++ workshop/exercises-P2_tests/exercise-2-4.js
@@ -10,12 +10,18 @@
 // getLetterAtIndex("abc", 0);   // a
 
 function getLetterAtIndex(str, index) {
-  // Your code here
+  console.log(str[index]);
+
+  return str[index];
 }
 
 // Add 6 more test cases
-expect(getLetterAtIndex('hello', 4), 'o');
-expect(getLetterAtIndex('goodbye', 0), 'g');
+expect(getLetterAtIndex("hello", 4), "o");
+expect(getLetterAtIndex("goodbye", 0), "g");
+expect(getLetterAtIndex("farewell"), undefined);
+expect(getLetterAtIndex("feature", -1), undefined);
+expect(getLetterAtIndex("square", "circle"), undefined);
+expect(getLetterAtIndex("six", 6), undefined);
 
 /**
  * -------------------------------------------------------------------
@@ -24,7 +30,7 @@ expect(getLetterAtIndex('goodbye', 0), 'g');
  */
 function expect(result, value) {
   if (result === value) {
-    console.log('✅ Test succeeded');
+    console.log("✅ Test succeeded");
   } else {
     console.log(`⛔️ Expected “${result}” to equal “${value}”`);
   }
